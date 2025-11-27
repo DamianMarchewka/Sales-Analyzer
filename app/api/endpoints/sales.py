0c2032f0fa76c3a49b943d15baf0b9fc9d9f6bfb
Damian Marchewka <enki6@o2.pl>
2025-11-27 11:33:49 +0100

    Add pandas logic to sales.py

--- app/api/endpoints/sales.py
+++ app/api/endpoints/sales.py
@@ -1,6 +1,4 @@
-from fastapi import APIRouter
-from fastapi import UploadFile
-from fastapi import File
+from fastapi import APIRouter, UploadFile, File
 import pandas as pd
 from io import BytesIO
 
@@ -8,10 +6,27 @@ router = APIRouter()
 
 @router.post("/upload/")
 async def upload_sales(file: UploadFile = File(...)):
+    stats = {}
+    unique = {}
+
     content = await file.read()
     df = pd.read_csv(BytesIO(content))
+
+    numeric_cols = df.select_dtypes(include="number")
+    for col in numeric_cols:
+        stats[col] = {
+            "min": float(numeric_cols[col].min()),
+            "max": float(numeric_cols[col].max()),
+            "mean": float(numeric_cols[col].mean()),
+            "sum": float(numeric_cols[col].sum())
+        }
+    text_cols = df.select_dtypes(exclude="number")
+    for col in text_cols:
+        unique[col] = text_cols[col].astype(str).unique().tolist()
     return {
-        "rows": len(df),
-        "columns": len(df.columns),
-        "columns_name": df.columns.tolist()
+        "rows": int(len(df)),
+        "columns": int(len(df.columns)),
+        "columns_name": df.columns.tolist(),
+        "numeric_stats": stats,
+        "text_unique": unique
     }
